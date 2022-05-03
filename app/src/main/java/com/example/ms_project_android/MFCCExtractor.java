84059300bd3b14822cbf158512cc5f7f4f9717b1
domenVres <56541137+domenVres@users.noreply.github.com>
2022-05-03 20:26:45 +0200

    librosa-like feature extraction added

---
+++ app/src/main/java/com/example/ms_project_android/MFCCExtractor.java
@@ -0,0 +1,47 @@
+package com.example.ms_project_android;
+
+import com.jlibrosa.audio.JLibrosa;
+import com.jlibrosa.audio.exception.FileFormatNotSupportedException;
+import com.jlibrosa.audio.wavFile.WavFileException;
+
+import java.io.IOException;
+
+public class MFCCExtractor {
+    private JLibrosa jlibrosa;
+    private int n_mfcc;
+
+    public MFCCExtractor(int n_mfcc) {
+        this.jlibrosa = new JLibrosa();
+        this.n_mfcc = n_mfcc;
+    }
+
+    private float[] openFile(String file_path) {
+        this.jlibrosa = new JLibrosa();
+
+        float[] audioFeatureValues = new float[0];
+        try {
+            audioFeatureValues = this.jlibrosa.loadAndRead(file_path, -1, -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WavFileException e) {
+            e.printStackTrace();
+        } catch (FileFormatNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return audioFeatureValues;
+    }
+
+    public float[][] getMFCC(String file_path) {
+        float[] audioFeatureValues = this.openFile(file_path);
+
+        return this.jlibrosa.generateMFCCFeatures(audioFeatureValues, this.jlibrosa.getSampleRate(), this.n_mfcc);
+    }
+
+    public float[] getMeanMFCC(String file_path) {
+        float[][] mfccValues = getMFCC(file_path);
+        float[] meanMFCCValues = this.jlibrosa.generateMeanMFCCFeatures(mfccValues, mfccValues.length, mfccValues[0].length);
+
+        return meanMFCCValues;
+    }
+}
